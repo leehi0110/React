4707678f1d1cd1f1b4fcd9dff34bff71c21a3ad6
SeungNyungLee <maple0110@naver.com>
2021-07-27 11:52:47 +0900

    [React - Hooks] App.js

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,27 +1,38 @@
 // import logo from "./logo.svg";
-// import Counter from "./useState/couter";
-import React, { useState } from "react";
+// import Counter from "./useState/counter";
+// import Info from "./useState/info";
+// import React, { useState } from "react";
+// import Counter from "./useReducer/Counter";
+// import Info from "./useReducer/Info";
+// import Average from "./useMemo/Average";
+// import Average from "./useCallback/Average";
+// import Average from "./useRef/Average";
 import "./App.css";
-import Info from "./useState/info";
+import Info from "./customHook/Info";
 
 const App = () => {
   // return <Counter />;
+  // // useState
   // return <Info />;
-  const [visible, setVisible] = useState(false);
-
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setVisible(!visible);
-        }}
-      >
-        {visible ? "숨기기" : "보이기"}
-      </button>
-      <hr />
-      {visible && <Info />}
-    </div>
-  );
+  // const [visible, setVisible] = useState(false);
+  // return (
+  //   <div>
+  //     <button
+  //       onClick={() => {
+  //         setVisible(!visible);
+  //       }}
+  //     >
+  //       {visible ? "숨기기" : "보이기"}
+  //     </button>
+  //     <hr />
+  //     {visible && <Info />}
+  //   </div>
+  // );
+  // // useEffect
+  // return <Counter />;
+  // return <Info />;
+  // return <Average />;
+  return <Info />;
 };
 
 export default App;
