8a7e6de91f98c7978caacf111d832a4a5b15e63e
SeungNyungLee <maple0110@naver.com>
2021-07-27 11:51:05 +0900

    [React - Hooks] customHook

---
+++ hooks/src/customHook/Info.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import useInputs from './useInputs';
+
+const Info = () => {
+  const [state, onChange] = useInputs({
+    name: '',
+    nickname: ''
+  });
+  const {name, nickname} = state;
+
+  return (
+    <div>
+      <div>
+        <input value={name} name="name" onChange={onChange}/>
+        <input value={nickname} name="nickname" onChange={onChange}/>
+      </div>
+      <div>
+        <div>
+          <b>이름:</b> {name}
+        </div>
+        <div>
+          <b>닉네임:</b> {nickname}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
