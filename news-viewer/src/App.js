f3924e55011264e020a78b24be243f0cce8fdeb3
SeungNyungLee <maple0110@naver.com>
2021-08-03 20:03:31 +0900

    [React - news-viewer] news-viewer
    news api와 axios 비동기 통신을 사용해 기사(article)을 가져와 렌더링 해준다.
    navigation을 구성해 선택된 category에 맞는 기사들을 불러온다

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import NewsPage from './pages/NewsPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <Route path="/:category?" component={NewsPage} />;
+};
+
+// import React, { useState, useCallback } from 'react';
+// import NewsList from './components/NewsList';
+// import Categories from './components/Categories';
+
+// const App = () => {
+//   const [category, setCategory] = useState('all');
+//   const onSelect = useCallback((category) => setCategory(category), []);
+
+//   return (
+//     <>
+//       <Categories category={category} onSelect={onSelect} />
+//       <NewsList category={category} />
+//     </>
+//   );
+// };
+// // useState를 통해 선택된 카테고리를 관리하는 코드
 
 export default App;
---
+++ news-viewer/src/components/Categories.js
@@ -0,0 +1,137 @@
+import React from 'react';
+// import styled, { css } from 'styled-components';
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+
+const categories = [
+  {
+    name: 'all',
+    text: '전체 보기',
+  },
+  {
+    name: 'business',
+    text: '비즈니스',
+  },
+  {
+    name: 'entertainment',
+    text: '엔터테이먼트',
+  },
+  {
+    name: 'health',
+    text: '건강',
+  },
+  {
+    name: 'science',
+    text: '과학',
+  },
+  {
+    name: 'sports',
+    text: '스포츠',
+  },
+  {
+    name: 'technology',
+    text: '기술',
+  },
+];
+
+const CategoriesBlock = styled.div`
+  display: flex;
+  padding: 1rem;
+  width: 768px;
+  margin: 0 auto;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    overflow: auto;
+  }
+`;
+
+// const Category = styled.div`
+//   font-size: 1.125rem;
+//   cursor: pointer;
+//   white-space: pre;
+//   text-decoration: none;
+//   color: inherit;
+//   padding-bottom: 0.25rem;
+
+//   &:hover {
+//     color: #495057;
+//   }
+
+//   ${props =>
+//     props.active && css`
+//       font-weight: 600;
+//       border-bottom: 2px solid #22b8cf;
+//       color: #22b8cf;
+//       &:hover {
+//         color : #3bc9db;
+//       }
+//     `
+//   }
+
+//   & + & {
+//     margin-left: 1rem;
+//   }
+// `;
+
+const Category = styled(NavLink)`
+  font-size: 1.125rem;
+  cursor: pointer;
+  white-space: pre;
+  text-decoration: none;
+  color: inherit;
+  padding-bottom: 0.25rem;
+
+  &:hover {
+    color: #495057;
+  }
+
+  &.active {
+    font-weight: 600;
+    border-bottom: 2px solid #22b8cf;
+    color: #22b8cf;
+
+    &:hover {
+      color: #3bc9db;
+    }
+  }
+
+  & + & {
+    margin-left: 1rem;
+  }
+`;
+
+const Categories = () => {
+  return (
+    <CategoriesBlock>
+      {categories.map((c) => (
+        <Category
+          key={c.name}
+          activeClassName="active"
+          exact={c.name === 'all'}
+          to={c.name === 'all' ? '/' : `/${c.name}`}
+        >
+          {c.text}
+        </Category>
+      ))}
+    </CategoriesBlock>
+  );
+};
+
+// const Categories = ({ category, onSelect }) => {
+//   return (
+//     <CategoriesBlock>
+//       {categories.map((c) => (
+//         <Category
+//           key={c.name}
+//           active={category === c.name}
+//           onClick={() => onSelect(c.name)}
+//         >
+//           {c.text}
+//         </Category>
+//       ))}
+//     </CategoriesBlock>
+//   );
+// };
+// // props로 받은 값과 함수를 이용해 선택된 네비게이션을 바꾸는 방식
+
+export default Categories;
