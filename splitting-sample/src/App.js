b15098ef4cdf9da458ceeb4ea77623e71f251c09
SeungNyungLee <maple0110@naver.com>
2021-08-13 11:03:20 +0900

    [React - splitting-sample] splitting-sample
    React 내장 유틸인  React.lazy()와 Suspense를 이용한 코드 스플리팅
    
    SSR를 지원하는 @loadable/component 라이브러리를 이용한 코드 스플리팅
    preload를 이용해 특정 이벤트시 코드를 미리 불러오는 것을 가능하게 한다.
    또한, timeout등 여러 기능을 제공

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,12 +1,34 @@
+import React, { useState, Suspense } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import loadable from "@loadable/component";
+// const SplitMe = React.lazy(() => import("./SplitMe"));
+const SplitMe = loadable(() => import("./SplitMe"), {
+  fallback: <div>loading...</div>,
+});
 
 function App() {
+  const [visible, setVisible] = useState(Boolean);
+
+  const onClick = () => {
+    setVisible(true);
+  };
+  const onMouseOver = () => {
+    SplitMe.preload();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello React</p>
+        <p onClick={onClick} onMouseOver={onMouseOver}>
+          Hello React
+        </p>
+        {/* <Suspense fallback={<div>loadding...</div>}>
+          {visible && <SplitMe />}
+        </Suspense> */}
+        {/* React.laze() 와 Suspense를 사용한 방법 */}
+        {visible && <SplitMe />}
       </header>
     </div>
   );
