dfe782b1a3c27519d51abdba79a0ff9a3b71a83d
SeungNyungLee <40688638+leehi0110@users.noreply.github.com>
2021-08-29 23:13:47 +0900

    Update NewsList.js

--- news-viewer/src/components/NewsList.js
+++ news-viewer/src/components/NewsList.js
@@ -27,7 +27,7 @@ const NewsList = ({category}) => {
       try {
         const query = category === 'all' ? '': `&category=${category}`;
         const response = await axios.get(
-          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=b4570451e069499089e383f93a7cce3b`,
+          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=${apikey}`,
         );
         setArticles(response.data.articles);
       } catch(e) {
